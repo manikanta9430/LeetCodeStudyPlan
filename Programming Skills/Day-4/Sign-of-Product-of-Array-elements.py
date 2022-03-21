02513a01a2182bb7004a51798ee4228cfa3a3284
Manikanta Rayala <72917946+manikanta9430@users.noreply.github.com>
2022-03-21 20:46:29 +0530

    Create Sign-of-Product-of-Array-elements.py

---
+++ Programming Skills/Day-4/Sign-of-Product-of-Array-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        product = 1
+        for x in nums:
+            product *= x
+        if(product>0):
+            return 1
+        if(product<0):
+            return -1
+        return 0
