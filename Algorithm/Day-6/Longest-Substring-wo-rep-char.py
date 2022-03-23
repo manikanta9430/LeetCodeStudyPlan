c35c5ccfef34f7a1e9e90d69f3011c4636bed81a
Manikanta Rayala <72917946+manikanta9430@users.noreply.github.com>
2022-03-23 23:14:09 +0530

    Create Longest-Substring-wo-rep-char.py

---
+++ Algorithm/Day-6/Longest-Substring-wo-rep-char.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = {}
+        mx = left = 0
+        for right, c in enumerate(s):
+            if c in seen:
+                left = max(left, seen[c] + 1)
+            seen[c] = right
+            mx = max(mx, right-left+1)
+        return mx
