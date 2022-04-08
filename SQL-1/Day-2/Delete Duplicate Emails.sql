18b96590cdc796ffa5fc965a4c35feb8b02124dc
Manikanta Rayala <72917946+manikanta9430@users.noreply.github.com>
2022-04-08 21:50:26 +0530

    Create Delete Duplicate Emails.sql

---
+++ SQL-1/Day-2/Delete Duplicate Emails.sql
@@ -0,0 +1,43 @@
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+
+ 
+
+Write an SQL query to delete all the duplicate emails, keeping only one unique email with the smallest id. Note that you are supposed to write a DELETE statement and not a SELECT one.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Output: 
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
+
+
+DELETE p1 from Person p0 inner join Person p1 on p0.email=p1.email where p1.id>p0.id;
