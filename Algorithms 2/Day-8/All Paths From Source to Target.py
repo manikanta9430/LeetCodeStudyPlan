2ed668c59c4abc031a0733d0c2b9826539172e26
Manikanta Rayala <72917946+manikanta9430@users.noreply.github.com>
2022-04-08 22:05:26 +0530

    Create All Paths From Source to Target.py

---
+++ Algorithms 2/Day-8/All Paths From Source to Target.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def allPathsSourceTarget(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        #edges cases:
+        if not graph:
+            return []
+        
+        # build di-graph
+        d = {}
+        for i in range(len(graph)):
+            d[i] = graph[i] # one-way link
+        
+        # apply DFS on DAG
+        n = len(graph)
+        stack = [(0, [0])] # - storing the (node, and [the path leading to it])
+        res = []
+        while stack:
+            node, path = stack.pop()
+            # check leaf
+            if node == n - 1:
+                res.append(path)
+            # traverse rest
+            for nei in d[node]:
+                stack.append((nei, path+[nei]))
+        return res
