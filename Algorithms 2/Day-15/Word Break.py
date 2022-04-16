2df461992492cc92e27b6d57d9eb55cc344e7934
Manikanta Rayala <72917946+manikanta9430@users.noreply.github.com>
2022-04-16 18:52:06 +0530

    Update Word Break.py

--- Algorithms 2/Day-15/Word Break.py
+++ Algorithms 2/Day-15/Word Break.py
@@ -1,17 +1,12 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-		n = len(s)
-        word_set = set(wordDict)
-        word_length = [len(x) for x in wordDict]
-        min_length, max_length = min(word_length), max(word_length)
-		
-        if len(s) < min_length:
-            return False
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
         
-		dp = [False] * (n + 1)
-        dp[0] = True
-        for i in range(1, n - min_length + 2):
-            for j in range(i - 1 + min_length, min(i + max_length, n + 1)):
-                if dp[i - 1] and s[i - 1:j] in word_set:
-                    dp[j] = True
-        return dp[-1]
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i: i + len(w)] == w:
+                    dp[i] = dp[i + len(w)] 
+                if dp[i]:
+                    break
+        return dp[0]
