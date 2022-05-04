0c518907c042549855766eef57f12f69709332c7
Manikanta Rayala <72917946+manikanta9430@users.noreply.github.com>
2022-05-04 21:49:15 +0530

    Create 60. Permutation Sequence.py

---
+++ Randoms/Day-3/60. Permutation Sequence.py
@@ -0,0 +1,4 @@
+from itertools import permutations
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        return ''.join(map(str, list(permutations(range(1, n + 1)))[k - 1]))
