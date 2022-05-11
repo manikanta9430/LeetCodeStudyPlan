368d3cfa06bd5b7dc8fccff0512614b9ca87e3df
Manikanta Rayala <72917946+manikanta9430@users.noreply.github.com>
2022-05-11 22:26:30 +0530

    Update 179. Largest Number.py

--- Randoms/Day-10/179. Largest Number.py
+++ Randoms/Day-10/179. Largest Number.py
@@ -1,12 +1,9 @@
-class Solution(object):
-    def largestNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: str
-        """
-        
-        
-        
-        x = "".join(sorted(map(str,nums),cmp=lambda a,b: 1 if a+b>b+a else -1 if b+a>a+b else 0,reverse=True))
-        
-        return "0" if x.count("0") == len(x) else x
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = [str(i) for i in nums]
+        maxi = max(nums,key=lambda x:len(x))
+        max_len = len(maxi)
+        nums = sorted(nums, key = lambda x:str(x * (10*(max_len-len(x) + 1))),reverse = True)       
+        res = "".join(nums)
+        res = int(res)//1
+        return str(res)
